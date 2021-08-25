2c6af0d89a04c34f42c254d75c956b1c58764a3c
Wesley Wood <wesleyowood@gmail.com>
2021-08-24 19:43:05 -0600

    Final

--- js/application.js
+++ js/application.js
@@ -40,4 +40,34 @@ $(document).ready(function () {
     $('#total').each(function (i, ele) {
         price = updateTotalPrice(ele);
     });
+    $('.btn.remove').on('click', function (event) {
+        $(this).closest('.item').remove();
+        updateItemPrice();
+        updateTotalPrice();
+    });
+    $('#addItem').on('submit', function (event) {
+        event.preventDefault();
+        var product = $('.addProduct').val();
+        var price = $('.addPrice').val();
+
+        $('#list').append('<div class="row item">' + 
+        '<div class="col-xs-3 product">' + product + '</div>'
+        + '<div class="col-xs-3 price">' + parseFloat(price).toFixed(2) + '</div>'
+        + '<div class ="col-xs-3 qty">' + 'QTY' + '<input type="number" value="1"/>' + '</div>'
+        + '<div class ="col-xs-1 delete">' + '<button class="btn btn-light btn-sm remove">' + 'Delete' + '</button>' + '</div>'
+        + '<div class="col-xs-2 totalCost">' + '$--.--' + '</div>'
+        + '</div>');
+
+        updateItemPrice();
+        updateTotalPrice();
+
+        $('.btn.remove').on('click', function (event) {
+            $(this).closest('.item').remove();
+            updateItemPrice();
+            updateTotalPrice();
+        });
+
+        $('.addProduct').val('');
+        $('.addPrice').val('');
+    });
   });
